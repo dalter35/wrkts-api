28be919c80b1ee5f963fd5f3bcdc9be9ab39741e
david alter <dalter@singleplatform.com>
2016-09-30 23:42:40 -0400

    postgres conn settings

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
+import psycopg2
+import urlparse
 
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wrkts'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
