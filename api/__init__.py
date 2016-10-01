950ba10f727588c728120ee1afeb0c0f398ae43a
david alter <dalter@singleplatform.com>
2016-10-01 11:33:46 -0400

    heroku db config

--- api/__init__.py
+++ api/__init__.py
@@ -7,17 +7,17 @@ import urlparse
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wrkts'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+# urlparse.uses_netloc.append("postgres")
+# url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+# conn = psycopg2.connect(
+#     database=url.path[1:],
+#     user=url.username,
+#     password=url.password,
+#     host=url.hostname,
+#     port=url.port
+# )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
