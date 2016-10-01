6917f076262af0f55ef4aafef110533d1fcb0e8f
david alter <dalter@singleplatform.com>
2016-09-30 23:38:07 -0400

    update db conn

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wrkts'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wrkts'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
