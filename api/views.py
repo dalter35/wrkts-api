7f0f4540fbdbad884a2065f4f1ca9c8401ee8f92
david alter <dalter@singleplatform.com>
2016-10-02 14:32:32 -0400

    add basic http auth

--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
 from api import app
-from flask import request
+from flask import request, g
 from flask_restful import reqparse, abort, Api, Resource
+from flask_httpauth import HTTPBasicAuth
 from models import Users, Workouts, Lifts, Runs
 
 api = Api(app)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    user = Users.query.filter_by(email = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
 
 class UsersList(Resource):
 	def get(self):
@@ -20,8 +30,10 @@ class UsersList(Resource):
 		return user.as_dict(), 201
 
 class AddRuns(Resource):
+	decorators = [auth.login_required]
+
 	def post(self):
-		user = Users.query.filter_by(id=request.form['user_id']).first_or_404()
+		user = Users.query.filter_by(id=g.user.id).first_or_404()
 		print user.email + ' added a run'
 		
 		workout = Workouts(user)
@@ -48,13 +60,13 @@ class AddLifts(Resource):
 		return lift.as_dict(), 201
 
 class UserWorkouts(Resource):
-	def get(self, user_id):
-		# user = Users.query.filter_by(id=user_id).first_or_404()
-		workouts = Workouts.query.filter_by(user_id=user_id).all()
+	decorators = [auth.login_required]
+
+	def get(self):
+		workouts = Workouts.query.filter_by(user_id=g.user.id).all()
 		return [workout.as_dict() for workout in workouts]
-		# workouts = Workouts.query.filter_by(user_id=user.id)
 
 api.add_resource(UsersList, '/users')
 api.add_resource(AddRuns, '/runs')
 api.add_resource(AddLifts, '/lifts')
-api.add_resource(UserWorkouts, '/workouts/<int:user_id>')
+api.add_resource(UserWorkouts, '/workouts')
